f9f3b90b75af56d923501aa1236930063a2b3c35
kou <chko22069@g.nihon-u.ac.jp>
2023-12-03 04:27:19 +0900

    iss 場所表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+
+async function fetchProducts(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  const filteredData = {
+    latitude: data.latitude,
+    longitude: data.longitude,
+  };
+
+  console.log(filteredData);
+
+  return filteredData;
+}
+
 export default function App() {
-    return (
-      <div>
-        <h1>Hello, World!</h1>
-      </div>
-    );
-  }
+  const iss_url ="https://api.wheretheiss.at/v1/satellites/25544";
+  const [data, setData] = useState(); 
+
+  useEffect(() => {
+    const fetchData = async () => {
+        const newContent = await fetchProducts(iss_url);
+        setData(newContent);
+      };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h1>where is iss</h1>
+      {data && (
+        <>
+          <p>緯度: {data.latitude}</p>
+          <p>経度: {data.longitude}</p>
+        </>
+      )}
+    </>
+  );
+}
