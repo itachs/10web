6aab70382b12a33a18480aabb4e364f4fa4ad84e
kou <chko22069@g.nihon-u.ac.jp>
2024-01-04 08:37:44 +0900

    軌道追加

--- src/App.jsx
+++ src/App.jsx
@@ -12,38 +12,57 @@ async function fetchProducts(url) {
     latitude: data.latitude,
     longitude: data.longitude,
   };
-
   console.log(filteredData);
 
   return filteredData;
 }
+
 function App() {
   const iss_url = "https://api.wheretheiss.at/v1/satellites/25544";
   const [data, setData] = useState(); // 初期値を null に設定
+  const[position,setPosition] = useState(data ? [data.latitude, data.longitude] : [0, 0]);
+  const [trajectoryData, setTrajectoryData] = useState([]);
   useEffect(() => {
-    const fetchData = async () => {
-      const newContent = await fetchProducts(iss_url);
-      setData(newContent);
+    const intervalId = setInterval(() => {
+      const fetchData = async () => {
+        console.log("aaaaa")
+        const newContent = await fetchProducts(iss_url);
+        setData(newContent);
+        console.log(newContent.latitude);
+        setPosition([newContent.latitude, newContent.longitude]);
+        console.log(position);
+        const newPoint = {
+          lat: newContent.latitude,
+          lng: newContent.longitude,
+        };
+        setTrajectoryData(prevData => [...prevData, newPoint]);
+      };
+  
+      fetchData();
+    }, 3000);
+    return () => {
+      clearInterval(intervalId);
     };
-
-    fetchData();
-  }, []);
-  const position = data ? [data.latitude, data.longitude] : [0, 0];
+  },[]);
+  
   return (
     <div className="App">
-       <h1>where is iss</h1>
+      <h1>where is iss</h1>
+      <h2>
+       This site provides real-time location information of the ISS (international space station)
+      </h2>
       {data && (
         <>
           <p>緯度: {data.latitude}</p>
           <p>経度: {data.longitude}</p>
         </>
       )}
-      <Map position={position} name="map" />
+      <Map position={position}  trajectoryData={trajectoryData} name="map" />
       <footer>
         <p>s5422069 板橋輝</p>
         <p>日本大学文理学部情報科学科 Webプログラミングの演習課題</p>
       </footer>
-    </div>    
+    </div>
   );
 }
 
--- src/Map.jsx
+++ src/Map.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
+import L from 'leaflet';
 import './Map.css';
-import L from 'leaflet'
-L.Icon.Default.imagePath = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/'
 
-export const Map = ({ position }) => {
-    // const position = [51.505, -0.09];
-    const zoom = 1;
-    return (
-      <MapContainer center={position} zoom={zoom}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+L.Icon.Default.imagePath = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/';
+
+export const Map = ({ position, trajectoryData }) => {
+  const zoom = 1;
+
+  return (
+    <MapContainer center={position} zoom={zoom} >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          ISS!!!<br/> Yayyyyyyyyy!!!!!!!
+        </Popup>
+      </Marker>
+      {trajectoryData && (
+        <Polyline
+          positions={trajectoryData.map(point => [point.lat, point.lng])}
+          color="blue"
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br/> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    )
-}
+      )}
+    </MapContainer>
+  );
+};
