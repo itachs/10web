e269e394b47a94c2d87ba14ebc4b9d7e307796d8
kou <chko22069@g.nihon-u.ac.jp>
2024-01-05 03:35:16 +0900

    現在地を取得

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,23 @@ import { useEffect, useState } from "react";
 async function fetchProducts(url) {
   const response = await fetch(url);
   const data = await response.json();
+  const altitude=data.altitude.toFixed(5);
   const filteredData = {
     latitude: data.latitude,
     longitude: data.longitude,
+    altitude: altitude,
+  };
+  console.log(filteredData);
+
+  return filteredData;
+}
+async function fetchContry(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  const filteredData = {
+    time:data.timezone_id,
+    offset:data.offset,
+    country: data.country_code,
   };
   console.log(filteredData);
 
@@ -19,16 +33,15 @@ async function fetchProducts(url) {
 
 function App() {
   const iss_url = "https://api.wheretheiss.at/v1/satellites/25544";
-  const [data, setData] = useState(); // 初期値を null に設定
+  const [data, setData] = useState(); 
+  const [countryData,setCountryData] = useState();
   const[position,setPosition] = useState(data ? [data.latitude, data.longitude] : [0, 0]);
   const [trajectoryData, setTrajectoryData] = useState([]);
   useEffect(() => {
     const intervalId = setInterval(() => {
       const fetchData = async () => {
-        console.log("aaaaa")
         const newContent = await fetchProducts(iss_url);
         setData(newContent);
-        console.log(newContent.latitude);
         setPosition([newContent.latitude, newContent.longitude]);
         console.log(position);
         const newPoint = {
@@ -36,8 +49,9 @@ function App() {
           lng: newContent.longitude,
         };
         setTrajectoryData(prevData => [...prevData, newPoint]);
+        const newCountry = await fetchContry("https://api.wheretheiss.at/v1/coordinates/"+newContent.latitude+","+newContent.longitude);
+        setCountryData(newCountry);
       };
-  
       fetchData();
     }, 3000);
     return () => {
@@ -55,8 +69,19 @@ function App() {
         <>
           <p>緯度: {data.latitude}</p>
           <p>経度: {data.longitude}</p>
+          {countryData && countryData.country !== "??" && (
+            <>
+              <p>現在地: {countryData.country} {data.altitude}Km上空</p>
+            </>
+          )}
+          {countryData && countryData.country == "??" &&(
+            <>
+              <p>現在地: SEA {data.altitude}Km上空</p>
+            </>
+      )}
         </>
       )}
+      
       <Map position={position}  trajectoryData={trajectoryData} name="map" />
       <footer>
         <p>s5422069 板橋輝</p>
